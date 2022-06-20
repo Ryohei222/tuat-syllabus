673b40d5c25ce9b397dcf2a1b7584ee801233680
Ryohei Kobayashi <kobaryo222912@gmail.com>
2022-06-20 17:22:23 +0900

    各科目の表示

--- app/course.py
+++ app/course.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request
-from app.db import 
+from flask import Blueprint, request, render_template
+from app.db import get_course_from_code
 bp = Blueprint("search", __name__, url_prefix="/course")
 
 @bp.route('/<string:code>', methods = ['GET'])
 def course(code):
-    c = 
+    c = get_course_from_code(code)
+    return render_template('code.html', code)   
