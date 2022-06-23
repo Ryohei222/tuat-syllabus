aab9cbc4a137ed69904b413b19c109ed4b604d4d
Ryohei <kobaryo222912@gmail.com>
2022-06-23 09:43:29 +0900

    tuat-syllabus を URL の prefix にした

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True) # instance/ の中にある config.py を読み込めるようになる
     app.config.from_mapping(
+        # APPLICATION_ROOT="/tuat-syllabus/",
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "temp.sqlite3")
     )
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,12 +1,7 @@
 wsgi_app = 'app:create_app()'
-# chdir = '/work/python/gunic/gu'
-# daemon = False
-raw_env = [
-    'ENV_TYPE=dev',
-    'HOGEHOGE_KEY=xxxxxxxxxxxxxxxxxxxxxxxxx'
-]
-
 bind = '0.0.0.0:8000'
 workers = 1
 proc_name = 'tuat-syllabus'
-loglevel = 'info'
+loglevel = 'debug'
+
+# chdir = '/work/python/gunic/gu'
